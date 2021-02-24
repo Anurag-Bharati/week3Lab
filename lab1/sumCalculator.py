140a5788e7446947b3e18619cf13869251bc9571
Anurag Bharati <anuragbharati26@gmail.com>
2021-02-24 11:16:05 +0545

    code to add 3 vars

---
+++ lab1/sumCalculator.py
@@ -0,0 +1,8 @@
+#  WAP that takes three no and prints their sum. Every no is given on a separate line.
+
+numA = float(input("\nEnter the first no: "))
+numB = float(input("\nEnter the second no: "))
+numC = float(input("\nEnter the third no: "))
+
+sumTotal = numA + numB + numC
+print(sumTotal)

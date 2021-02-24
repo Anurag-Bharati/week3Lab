9bfa3fe402073ab651d4806396396f64f58f039b
Anurag Bharati <anuragbharati26@gmail.com>
2021-02-24 11:22:26 +0545

    code to add 3 vars

---
+++ lab1/areaTriangle.py
@@ -0,0 +1,6 @@
+# WAP that reads the length of the base and the height of a right-angle triangle and print the area
+
+base = float(input("\nEnter the base of triangle: "))
+height = float(input("\nEnter the height of triangle: "))
+areaTri = 0.5*(base*height)
+print(areaTri)

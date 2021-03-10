ebb864db44695f4097e2417546ef12ab09b97974
Anurag Bharati <anuragbharati26@gmail.com>
2021-03-10 11:34:17 +0545

    week4.1

---
+++ lab1/to find positive int.py
@@ -0,0 +1,10 @@
+# WAP to find the sum of the first "n" positive int.
+
+n = int(input("Enter a number: "))
+nsum = (n*(n+1)) / 2
+print(f"The sun of given positive int is {nsum}")
+
+nArrey = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+nsum = sum(nArrey)
+print(f"The sun of given positive int is {nsum}")
+
